a421a0c5f971de2fead09d168ee4d69e600c40ee
Asad-Rao <113167578+Asad-Rao@users.noreply.github.com>
2023-01-20 12:18:33 +0500

    Add files via upload

---
+++ jsslidedownhamburgermeneu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".container").click(function(){
+      $(".nav-items").slideToggle("slow");
+      $(".bar1").toggleClass("change1");
+  $(".bar2").toggleClass("change2");
+  $(".bar3").toggleClass("change3");
+    })});
+
