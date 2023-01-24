030e6b6acad5cb6486268d610e1deb53eb9898d0
Asad-Rao <113167578+Asad-Rao@users.noreply.github.com>
2023-01-24 13:39:21 +0500

    Update jsslidedownhamburgermeneu.js

--- jsslidedownhamburgermeneu.js
+++ jsslidedownhamburgermeneu.js
@@ -3,9 +3,18 @@ $(document).ready(function(){
         $(".shopoutlets").toggleClass("displayblock");
         $(".quality").toggleClass("displayblock");
         $(".cheapproducts").toggleClass("displayblock");
-      $(".nav-items").slideToggle(1000);
+      $(".nav-items").slideToggle(500);
       $(".bar1").toggleClass("change1");
   $(".bar2").toggleClass("change2");
   $(".bar3").toggleClass("change3");
     })});
-
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 20) {
+         $('#nav-id').addClass('navbarclass');
+         $('#nav-items').addClass('navbarclass');
+      }
+         else {
+          $('#nav-id').removeClass('navbarclass');
+          $('#nav-items').removeClass('navbarclass');
+       }
+  });
